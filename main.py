72adb6c545003fcc347e010cabc5978ab6cf6d16
Christopher Martin <martincdev@gmail.com>
2024-03-19 13:17:02 -0500

    Formatted file

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
 def get_word_count(text):
     words = text.split()
     return len(words)
+
 
 def get_char_count(text):
     char_dictionary = {}
     lowercase_text = text.lower()
     for char in lowercase_text:
-         if char in char_dictionary:
-                char_dictionary[char] += 1
-         else:
-             char_dictionary[char] = 1
+        if char in char_dictionary:
+            char_dictionary[char] += 1
+        else:
+            char_dictionary[char] = 1
 
     return char_dictionary
+
 
 def main():
     book_path = "books/frankenstein.txt"
